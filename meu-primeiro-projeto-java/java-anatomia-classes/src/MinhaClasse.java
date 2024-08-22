e23a5c8af4bd9f80a495f66f9ff4ec57efdb4cdb
João Paulo Silva <126629292+DevJoaoSillva@users.noreply.github.com>
2024-08-22 16:30:00 -0300

    Anatomia-Classes-2-commit

--- meu-primeiro-projeto-java/java-anatomia-classes/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,12 @@
 public class MinhaClasse {
     public static void main(String[] args){
-        System.out.print("Olá Turma, sejam bem vindos!");
-    }
+        String primeiroNome = "João";
+        String segundoNome = "Paulo";
 
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
